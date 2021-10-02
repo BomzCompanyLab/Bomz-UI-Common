7c319fd8ec67181984e8a78065d9b1d62f0bd226
bom bom <bom@bomz.app>
2021-10-02 14:16:48 +0000

    ver 1.1

---
+++ src/kr/co/bomz/cmn/ui/BomzUI.java
@@ -0,0 +1,18 @@
+package kr.co.bomz.cmn.ui;
+
+/**
+ * 	봄즈의 Control UI
+ *  
+ * @author Bomz
+ * @version 1.0
+ * @since 1.0
+ *
+ */
+public interface BomzUI {
+
+	/**
+	 * 	FXML 에서 setStyle="..." att 설정 값 적용
+	 * @param styleClass		CSS 스타일 클래스명
+	 */
+	public void setStyleClass(String styleClass);
+}
