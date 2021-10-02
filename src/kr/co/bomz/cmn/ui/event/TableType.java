d334369b1b3f967663965e7de09afc14e4011e37
bom bom <bom@bomz.app>
2021-10-02 14:19:45 +0000

    새 파일 업로드

---
+++ src/kr/co/bomz/cmn/ui/event/TableType.java
@@ -0,0 +1,19 @@
+package kr.co.bomz.cmn.ui.event;
+
+/**
+ * 	테이블 이용 여부에 따른 타입
+ * 
+ * @author Bomz
+ * @version 1.0
+ * @since 1.0
+ *
+ */
+public enum TableType {
+
+	/**		비어있는 테이블		*/
+	EMPTY_TABLE,
+	/**		대기중인 테이블		*/
+	WAIT_TABLE,
+	/**		이용중인 테이블		*/
+	USE_TABLE
+}
