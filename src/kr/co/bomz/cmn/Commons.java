d37f9bf9d09c7eafc411af60cbbbe326c2d86a94
bom bom <bom@bomz.app>
2021-10-02 14:10:58 +0000

    ver 1.1

---
+++ src/kr/co/bomz/cmn/Commons.java
@@ -0,0 +1,21 @@
+package kr.co.bomz.cmn;
+
+/**
+ * 	공통 설정 값 정의
+ * 
+ * @author Bomz
+ * @version 1.0
+ * @since 1.0
+ *
+ */
+public class Commons {
+
+	/**	예/아니요로 표현되는 타입 중 '예' 		*/
+	public static final char TYPE_YES = 'Y';
+	
+	/**	예/아니요로 표현되는 타입 중 '아니요' 		*/
+	public static final char TYPE_NO = 'N';
+	
+	/**	공통 로거 아이디		*/
+	public static final String CMN_LOGGER_ID = "cmn";
+}
