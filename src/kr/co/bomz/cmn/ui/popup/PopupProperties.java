4839ce67b129e99d3c29f0233e5afecda765aec8
bom bom <bom@bomz.app>
2021-10-02 14:23:54 +0000

    ver 1.1

---
+++ src/kr/co/bomz/cmn/ui/popup/PopupProperties.java
@@ -0,0 +1,23 @@
+package kr.co.bomz.cmn.ui.popup;
+
+/**
+ * 	팝업 생성에 사용되는 아이디를 관리
+ * 
+ * @author Bomz
+ * @version 1.1
+ * @since 1.0
+ *
+ */
+public class PopupProperties {
+
+	/**		'예' 버튼 클릭 시 호출될 이벤트 핸들러 아이디		*/
+	public static final String POPUP_OK_EVENT_HANDLER_ID = "id_confirm_ok_hd";
+	
+	/**		'아니요' 버튼 클릭 시 호출될 이벤트 핸들러 아이디		*/
+	public static final String POPUP_CANCEL_EVENT_HANDLER_ID = "id_confirm_cancel_hd";
+	
+
+	/**		팝업 버튼 스타일			*/
+	public static final String POPUP_BT_STY_ID = "id_ok_pop_bt_sty";
+	
+}
